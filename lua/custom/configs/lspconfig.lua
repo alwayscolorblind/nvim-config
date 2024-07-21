cc6371e1a58fb0a75d86acc2f71f7fb5fc8d9ff6
a <55889540+alwayscolorblind@users.noreply.github.com>
2024-07-21 21:01:54 +0300

    Update lspconfig.lua

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -22,9 +22,9 @@ require("typescript-tools").setup {
   capabilities = capabilities,
   settings = {
     -- spawn additional tsserver instance to calculate diagnostics on it
-    separate_diagnostic_server = true,
+    separate_diagnostic_server = false,
     -- "change"|"insert_leave" determine when the client asks the server about diagnostic
-    publish_diagnostic_on = "insert_leave",
+    publish_diagnostic_on = "change",
     -- array of strings("fix_all"|"add_missing_imports"|"remove_unused"|
     -- "remove_unused_imports"|"organize_imports") -- or string "all"
     -- to include all supported code actions
@@ -39,6 +39,18 @@ require("typescript-tools").setup {
     -- this value is passed to: https://nodejs.org/api/cli.html#--max-old-space-sizesize-in-megabytes
     -- memory limit in megabytes or "auto"(basically no limit)
     tsserver_max_memory = "auto",
+
+    inlayHints = {
+      includeInlayParameterNameHints = "all",
+      includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+      includeInlayFunctionParameterTypeHints = true,
+      includeInlayVariableTypeHints = true,
+      includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+      includeInlayPropertyDeclarationTypeHints = true,
+      includeInlayFunctionLikeReturnTypeHints = true,
+      includeInlayEnumMemberValueHints = true,
+    },
+
     -- described below
     tsserver_format_options = {
       insertSpaceAfterOpeningAndBeforeClosingNonemptyBraces = true,
@@ -48,6 +60,15 @@ require("typescript-tools").setup {
     },
     tsserver_file_preferences = {
       quotePreference = "single",
+      disableSuggestions = true,
+      includeInlayParameterNameHints = 'literals',
+      includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+      includeInlayFunctionParameterTypeHints = true,
+      includeInlayVariableTypeHints = true,
+      includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+      includeInlayPropertyDeclarationTypeHints = true,
+      includeInlayFunctionLikeReturnTypeHints = true,
+      includeInlayEnumMemberValueHints = true,
     },
     -- locale of all tsserver messages, supported locales you can find here:
     -- https://github.com/microsoft/TypeScript/blob/3c221fc086be52b19801f6e8d82596d04607ede6/src/compiler/utilitiesPublic.ts#L620
@@ -58,7 +79,7 @@ require("typescript-tools").setup {
     -- CodeLens
     -- WARNING: Experimental feature also in VSCode, because it might hit performance of server.
     -- possible values: ("off"|"all"|"implementations_only"|"references_only")
-    code_lens = "off",
+    code_lens = "implementations_only",
     -- by default code lenses are displayed on all referencable values and for some of you it can
     -- be too much this option reduce count of them by removing member references from lenses
     disable_member_code_lens = true,
@@ -71,127 +92,6 @@ require("typescript-tools").setup {
     },
   },
 }
-
--- local root_has_file = function(files)
---   return function(utils)
---     return utils.root_has_file(files)
---   end
--- end
---
--- local eslint_root_files = { ".eslintrc", ".eslintrc.js", ".eslintrc.json" }
--- local prettier_root_files = { ".prettierrc", ".prettierrc.js", ".prettierrc.json" }
--- local stylua_root_files = { "stylua.toml", ".stylua.toml" }
--- local elm_root_files = { "elm.json" }
---
--- local opts = {
---   eslint_formatting = {
---     condition = function(utils)
---       local has_eslint = root_has_file(eslint_root_files)(utils)
---       local has_prettier = root_has_file(prettier_root_files)(utils)
---       return has_eslint and not has_prettier
---     end,
---   },
---   eslint_diagnostics = {
---     condition = root_has_file(eslint_root_files),
---   },
---   prettier_formatting = {
---     condition = root_has_file(prettier_root_files),
---   },
---   stylua_formatting = {
---     condition = root_has_file(stylua_root_files),
---   },
---   elm_format_formatting = {
---     condition = root_has_file(elm_root_files),
---   },
--- }
---
--- null_ls.setup {
---   on_attach = on_attach,
---   capabilities = capabilities,
---   sources = {
---     null_ls.builtins.diagnostics.eslint_d.with(opts.eslint_diagnostics),
---     null_ls.builtins.formatting.eslint_d.with(opts.eslint_formatting),
---     null_ls.builtins.formatting.prettier.with(opts.prettier_formatting),
---     null_ls.builtins.formatting.stylua.with(opts.stylua_formatting),
---     null_ls.builtins.formatting.elm_format.with(opts.elm_format_formatting),
---     null_ls.builtins.code_actions.eslint_d.with(opts.eslint_diagnostics),
---   },
--- }
---
--- eslint.setup {
---   bin = "eslint", -- or `eslint_d`
---   on_attach = on_attach,
---   capabilities = capabilities,
---   code_actions = {
---     enable = true,
---     apply_on_save = {
---       enable = true,
---       types = { "directive", "problem", "suggestion", "layout" },
---     },
---     disable_rule_comment = {
---       enable = true,
---       location = "separate_line", -- or `same_line`
---     },
---   },
---   diagnostics = {
---     enable = true,
---     report_unused_disable_directives = false,
---     run_on = "type", -- or `save`
---   },
--- }
---
--- prettier.setup {
---   bin = "prettier",
---   filetypes = {
---     "css",
---     "graphql",
---     "html",
---     "javascript",
---     "javascriptreact",
---     "json",
---     "less",
---     "markdown",
---     "scss",
---     "typescript",
---     "typescriptreact",
---     "yaml",
---   },
---   ["null-ls"] = {
---     condition = function()
---       return prettier.config_exists {
---         -- if `false`, skips checking `package.json` for `"prettier"` key
---         check_package_json = true,
---       }
---     end,
---     runtime_condition = function(params)
---       -- return false to skip running prettier
---       return true
---     end,
---     timeout = 5000,
---   },
---   cli_options = {
---     arrow_parens = "always",
---     bracket_spacing = true,
---     bracket_same_line = false,
---     embedded_language_formatting = "auto",
---     end_of_line = "lf",
---     html_whitespace_sensitivity = "css",
---     -- jsx_bracket_same_line = false,
---     jsx_single_quote = false,
---     print_width = 110,
---     prose_wrap = "preserve",
---     quote_props = "as-needed",
---     semi = true,
---     single_attribute_per_line = false,
---     single_quote = true,
---     tab_width = 2,
---     trailing_comma = "es5",
---     use_tabs = false,
---     vue_indent_script_and_style = false,
---
---     config_precedence = "prefer-file", -- or "cli-override" or "file-override"
---   },
--- }
 
 lspconfig.emmet_ls.setup {
   on_attach = on_attach,
@@ -223,7 +123,6 @@ lspconfig.gopls.setup {
 --   init_options = {
 --     preferences = {
 --       disableSuggestions = true,
---       quotePreference = "single",
 --     },
 --   },
 --   settings = {
